6a9ce1f5bc31bd1a38d34e36ea24972028eb176c
suryas-1805 <suryas18051998@gmail.com>
2025-06-27 17:21:35 +0530

    changes

--- src/pages/Notifications/Notifications.tsx
+++ src/pages/Notifications/Notifications.tsx
@@ -1,7 +1,113 @@
-import React from 'react';
+import { COLORS, FONTS } from '@/constants/uiConstants';
+import backImg from '../../assets/icons/common/back_arrow.png';
+import { Card } from '@/components/ui/card';
+import { Button } from '@/components/ui/button';
 
 const Notifications = () => {
-	return <div>Notifications</div>;
+	return (
+		<>
+			<div className='py-8'>
+				<div className='flex items-center gap-6'>
+					<div
+						className='cursor-pointer p-2 rounded-md'
+						style={{
+							boxShadow: `
+								rgba(255, 255, 255, 0.7) 5px 5px 4px, 
+								rgba(189, 194, 199, 0.75) 2px 2px 3px inset
+								`,
+						}}
+					>
+						<img src={backImg} alt='back' />
+					</div>
+					<p style={{ ...FONTS.heading_01 }}>Notification</p>
+					<span style={{ ...FONTS.heading_06, marginLeft: '320px' }}>
+						0 Message / 0 Unread
+					</span>
+				</div>
+				<div className='grid md:grid-cols-2 gap-6 w-full mt-6'>
+					<Card
+						className='relative bg-[#ebeff3] px-5'
+						style={{
+							boxShadow: `
+   
+       rgba(255, 255, 255, 0.7) -4px -4px 4px, 
+       rgba(189, 194, 199, 0.75) 5px 5px 4px
+      
+    `,
+						}}
+					>
+						<input
+							type='text'
+							style={{
+								boxShadow: `
+								rgba(255, 255, 255, 0.7) 5px 5px 4px, 
+								rgba(189, 194, 199, 0.75) 2px 2px 3px inset
+								`,
+								...FONTS.para_01,
+							}}
+							className='px-3 h-12 rounded-md'
+							placeholder='Search'
+						/>
+						<div className='flex flex-row gap-3 mb-6'>
+							<Button
+								className='bg-[#7b00ff]'
+								variant='outline'
+								style={{ ...FONTS.heading_06, color: COLORS.white }}
+							>
+								All
+							</Button>
+							<Button
+								className='bg-[#ebeff3]'
+								variant='outline'
+								style={{
+									boxShadow: `
+      rgba(255, 255, 255, 0.7) 5px 5px 4px, 
+      rgba(189, 194, 199, 0.75) 2px 2px 3px inset
+    `,
+								}}
+							>
+								Read
+							</Button>
+							<Button
+								className='bg-[#ebeff3]'
+								variant='outline'
+								style={{
+									boxShadow: `
+      rgba(255, 255, 255, 0.7) 5px 5px 4px, 
+      rgba(189, 194, 199, 0.75) 2px 2px 3px inset
+    `,
+								}}
+							>
+								Unread
+							</Button>
+						</div>
+						<Card
+							className='relative bg-[#ebeff3] h-[65px]'
+							style={{
+								boxShadow: `
+   
+       rgba(255, 255, 255, 0.7) -4px -4px 4px, 
+       rgba(189, 194, 199, 0.75) 5px 5px 4px
+      
+    `,
+							}}
+						></Card>
+					</Card>
+					<Card
+						className='relative bg-[#ebeff3] h-[231px]'
+						style={{
+							boxShadow: `
+   
+       rgba(255, 255, 255, 0.7) -4px -4px 4px, 
+       rgba(189, 194, 199, 0.75) 5px 5px 4px
+      
+    `,
+						}}
+					></Card>
+				</div>
+			</div>
+		</>
+	);
 };
 
 export default Notifications;
--- src/pages/Tickets/Tickets.tsx
+++ src/pages/Tickets/Tickets.tsx
@@ -1,315 +1,326 @@
 import { Button } from '@/components/ui/button';
-import { Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card';
+import {
+	Card,
+	CardAction,
+	CardContent,
+	CardDescription,
+	CardFooter,
+	CardHeader,
+	CardTitle,
+} from '@/components/ui/card';
 import { Dialog, DialogTrigger } from '@/components/ui/dialog';
-
 
 const Tickets = () => {
 	return (
-		 <div className="flex min-h-screen flex-col items-start justify-start px-8 py-2    ">
-      <h1 className="text-2xl font-bold mb-6">Ticket</h1>
+		<div className='flex min-h-screen flex-col items-start justify-start px-8 py-2    '>
+			<h1 className='text-2xl font-bold mb-6'>Ticket</h1>
 
-
-      <div className="flex flex-row gap-3 mb-6">
-        <Button className="bg-[#7b00ff]" variant="outline">All</Button>
-        <Button className="bg-[#ebeff3]"variant="outline"
-         style={{
-                        boxShadow: `
+			<div className='flex flex-row gap-3 mb-6'>
+				<Button className='bg-[#7b00ff]' variant='outline'>
+					All
+				</Button>
+				<Button
+					className='bg-[#ebeff3]'
+					variant='outline'
+					style={{
+						boxShadow: `
       rgba(255, 255, 255, 0.7) 5px 5px 4px, 
       rgba(189, 194, 199, 0.75) 2px 2px 3px inset
     `,
-                      }}
-                    >Open</Button>
-        <Button className="bg-[#ebeff3]" variant="outline"
-         style={{
-                        boxShadow: `
+					}}
+				>
+					Open
+				</Button>
+				<Button
+					className='bg-[#ebeff3]'
+					variant='outline'
+					style={{
+						boxShadow: `
       rgba(255, 255, 255, 0.7) 5px 5px 4px, 
       rgba(189, 194, 199, 0.75) 2px 2px 3px inset
     `,
-                      }}
-                    >Close</Button>
-      </div>
+					}}
+				>
+					Close
+				</Button>
+			</div>
 
-
-      <div className="grid  md:grid-cols-2 gap-6 w-full ">
-
-        <Card
-          className="relative bg-[#ebeff3] h-[231px]"
-          style={{
-            boxShadow: `
+			<div className='grid  md:grid-cols-2 gap-6 w-full '>
+				<Card
+					className='relative bg-[#ebeff3] h-[231px]'
+					style={{
+						boxShadow: `
    
        rgba(255, 255, 255, 0.7) -4px -4px 4px, 
        rgba(189, 194, 199, 0.75) 5px 5px 4px
       
     `,
-          }}
-
-        >
-
-
-          <CardHeader>
-            <div className="flex justify-between items-center">
-              <div>
-                <CardTitle className="text-chart-3">TICKET #ANTITS01</CardTitle>
-
-              </div>
-              <CardAction>
-                <Dialog>
-                  <DialogTrigger>
-                    <Button
-                      className="bg-[#ebeff3]"
-                      variant="outline"
-                      style={{
-                        boxShadow: `
+					}}
+				>
+					<CardHeader>
+						<div className='flex justify-between items-center'>
+							<div>
+								<CardTitle className='text-chart-3'>TICKET #ANTITS01</CardTitle>
+							</div>
+							<CardAction>
+								<Dialog>
+									<DialogTrigger>
+										<Button
+											className='bg-[#ebeff3]'
+											variant='outline'
+											style={{
+												boxShadow: `
       rgba(255, 255, 255, 0.7) 5px 5px 4px, 
       rgba(189, 194, 199, 0.75) 2px 2px 3px inset
     `,
-                      }}
-                    >
-                      28 April
-                    </Button>
+											}}
+										>
+											28 April
+										</Button>
+									</DialogTrigger>
+								</Dialog>
+							</CardAction>
+						</div>
+					</CardHeader>
+					<CardContent>
+						<p className='font-semibold'>
+							This ticket created from student mobile app
+						</p>
 
+						<p>If you can this yes successfully mobile app an android</p>
+					</CardContent>
 
-                  </DialogTrigger>
-                </Dialog>
-              </CardAction>
-            </div>
-          </CardHeader>
-          <CardContent>
-            <p className="font-semibold">This ticket created from student mobile app</p>
-
-            <p>If you can this yes successfully mobile app an android</p>
-          </CardContent>
-
-          <CardFooter>
-            <div className="flex w-full justify-between items-center">
-
-
-              <p>1</p>
-              <CardAction >
-                <Dialog>
-                  <DialogTrigger className="text-sm font-medium text-primary shadow-md">
-                    <Button className="bg-[#ebeff3] w-30" variant="outline"
-                      style={{
-                        boxShadow: `
+					<CardFooter>
+						<div className='flex w-full justify-between items-center'>
+							<p>1</p>
+							<CardAction>
+								<Dialog>
+									<DialogTrigger className='text-sm font-medium text-primary shadow-md'>
+										<Button
+											className='bg-[#ebeff3] w-30'
+											variant='outline'
+											style={{
+												boxShadow: `
       rgba(255, 255, 255, 0.7) 5px 5px 4px, 
       rgba(189, 194, 199, 0.75) 2px 2px 3px inset
     `,
-                      }}
-                    >Opend</Button>
-
-                  </DialogTrigger>
-                </Dialog>
-              </CardAction>
-            </div>
-          </CardFooter>
-
-        </Card>
-        <Card
-          className="relative bg-[#ebeff3] h-[231px]"
-          style={{
-            boxShadow: `
+											}}
+										>
+											Opend
+										</Button>
+									</DialogTrigger>
+								</Dialog>
+							</CardAction>
+						</div>
+					</CardFooter>
+				</Card>
+				<Card
+					className='relative bg-[#ebeff3] h-[231px]'
+					style={{
+						boxShadow: `
    
        rgba(255, 255, 255, 0.7) -4px -4px 4px, 
        rgba(189, 194, 199, 0.75) 5px 5px 4px
       
     `,
-          }}
-
-        >
-
-          <CardHeader>
-            <div className="flex justify-between items-center">
-              <div>
-                <CardTitle className="text-chart-3">TICKET #ANTITS01</CardTitle>
-                <CardDescription></CardDescription>
-              </div>
-              <CardAction>
-                <Dialog>
-                  <DialogTrigger>
-                     <Button
-                      className="bg-[#ebeff3]"
-                      variant="outline"
-                      style={{
-                        boxShadow: `
+					}}
+				>
+					<CardHeader>
+						<div className='flex justify-between items-center'>
+							<div>
+								<CardTitle className='text-chart-3'>TICKET #ANTITS01</CardTitle>
+								<CardDescription></CardDescription>
+							</div>
+							<CardAction>
+								<Dialog>
+									<DialogTrigger>
+										<Button
+											className='bg-[#ebeff3]'
+											variant='outline'
+											style={{
+												boxShadow: `
        rgba(255, 255, 255, 0.7) 5px 5px 4px, 
        rgba(189, 194, 199, 0.75) 2px 2px 3px inset
     `,
-                      }}
-                    >
-                      28 April
-                    </Button>
-                  </DialogTrigger>
-                </Dialog>
-              </CardAction>
-            </div>
-          </CardHeader>
-          <CardContent>
-            <p className="font-semibold">This ticket created from student mobile app</p>
+											}}
+										>
+											28 April
+										</Button>
+									</DialogTrigger>
+								</Dialog>
+							</CardAction>
+						</div>
+					</CardHeader>
+					<CardContent>
+						<p className='font-semibold'>
+							This ticket created from student mobile app
+						</p>
 
-            <p>If you can this yes successfully mobile app an android</p>
-          </CardContent>
-          <CardFooter>
-            <div className="flex w-full justify-between items-center">
-
-
-              <p>1</p>
-              <CardAction >
-                <Dialog>
-                  <DialogTrigger className="text-sm font-medium text-primary shadow-md">
-                    <Button className="bg-[#ebeff3] w-30" variant="outline"
-                      style={{
-                        boxShadow: `
+						<p>If you can this yes successfully mobile app an android</p>
+					</CardContent>
+					<CardFooter>
+						<div className='flex w-full justify-between items-center'>
+							<p>1</p>
+							<CardAction>
+								<Dialog>
+									<DialogTrigger className='text-sm font-medium text-primary shadow-md'>
+										<Button
+											className='bg-[#ebeff3] w-30'
+											variant='outline'
+											style={{
+												boxShadow: `
        rgba(255, 255, 255, 0.7) 5px 5px 4px, 
        rgba(189, 194, 199, 0.75) 2px 2px 3px inset
     `,
-                      }}
-                    >Opend</Button>
-
-                  </DialogTrigger>
-                </Dialog>
-              </CardAction>
-            </div>
-          </CardFooter>
-
-        </Card>
-        <Card
-          className="relative bg-[#ebeff3] h-[231px]"
-          style={{
-            boxShadow: `
+											}}
+										>
+											Opend
+										</Button>
+									</DialogTrigger>
+								</Dialog>
+							</CardAction>
+						</div>
+					</CardFooter>
+				</Card>
+				<Card
+					className='relative bg-[#ebeff3] h-[231px]'
+					style={{
+						boxShadow: `
    
        rgba(255, 255, 255, 0.7) -4px -4px 4px, 
        rgba(189, 194, 199, 0.75) 5px 5px 4px
       
     `,
-          }}
-
-        >
-
-          <CardHeader>
-            <div className="flex justify-between items-center">
-              <div>
-                <CardTitle className="text-chart-3">TICKET #ANTITS01</CardTitle>
-                <CardDescription></CardDescription>
-              </div>
-              <CardAction>
-                <Dialog>
-                  <DialogTrigger>
-                    <Button
-                      className="bg-[#ebeff3]"
-                      variant="outline"
-                      style={{
-                        boxShadow: `
+					}}
+				>
+					<CardHeader>
+						<div className='flex justify-between items-center'>
+							<div>
+								<CardTitle className='text-chart-3'>TICKET #ANTITS01</CardTitle>
+								<CardDescription></CardDescription>
+							</div>
+							<CardAction>
+								<Dialog>
+									<DialogTrigger>
+										<Button
+											className='bg-[#ebeff3]'
+											variant='outline'
+											style={{
+												boxShadow: `
        rgba(255, 255, 255, 0.7) 5px 5px 4px, 
        rgba(189, 194, 199, 0.75) 2px 2px 3px inset
     `,
-                      }}
-                    >
-                      28 April
-                    </Button>
-                  </DialogTrigger>
-                </Dialog>
-              </CardAction>
-            </div>
-          </CardHeader>
-          <CardContent>
-            <p className="font-semibold">This ticket created from student mobile app</p>
+											}}
+										>
+											28 April
+										</Button>
+									</DialogTrigger>
+								</Dialog>
+							</CardAction>
+						</div>
+					</CardHeader>
+					<CardContent>
+						<p className='font-semibold'>
+							This ticket created from student mobile app
+						</p>
 
-            <p>If you can this yes successfully mobile app an android</p>
-          </CardContent>
-          <CardFooter>
-            <div className="flex w-full justify-between items-center">
-
-
-              <p>1</p>
-              <CardAction >
-                <Dialog>
-                  <DialogTrigger className="text-sm font-medium text-primary shadow-md">
-                    <Button className="bg-[#ebeff3] w-30" variant="outline"
-                      style={{
-                        boxShadow: `
+						<p>If you can this yes successfully mobile app an android</p>
+					</CardContent>
+					<CardFooter>
+						<div className='flex w-full justify-between items-center'>
+							<p>1</p>
+							<CardAction>
+								<Dialog>
+									<DialogTrigger className='text-sm font-medium text-primary shadow-md'>
+										<Button
+											className='bg-[#ebeff3] w-30'
+											variant='outline'
+											style={{
+												boxShadow: `
        rgba(255, 255, 255, 0.7) 5px 5px 4px, 
        rgba(189, 194, 199, 0.75) 2px 2px 3px inset
     `,
-                      }}
-                    >Opend</Button>
-
-                  </DialogTrigger>
-                </Dialog>
-              </CardAction>
-            </div>
-          </CardFooter>
-        </Card>
-        <Card
-          className="relative bg-[#ebeff3] h-[231px]"
-          style={{
-            boxShadow: `
+											}}
+										>
+											Opend
+										</Button>
+									</DialogTrigger>
+								</Dialog>
+							</CardAction>
+						</div>
+					</CardFooter>
+				</Card>
+				<Card
+					className='relative bg-[#ebeff3] h-[231px]'
+					style={{
+						boxShadow: `
    
        rgba(255, 255, 255, 0.7) -4px -4px 4px, 
        rgba(189, 194, 199, 0.75) 5px 5px 4px
       
     `,
-          }}
-
-        >
-
-          <CardHeader>
-            <div className="flex justify-between items-center">
-              <div>
-                <CardTitle className="text-chart-3">TICKET #ANTITS01</CardTitle>
-                <CardDescription></CardDescription>
-              </div>
-              <CardAction>
-                <Dialog>
-                  <DialogTrigger>
-                     <Button
-                      className="bg-[#ebeff3]"
-                      variant="outline"
-                      style={{
-                        boxShadow: `
+					}}
+				>
+					<CardHeader>
+						<div className='flex justify-between items-center'>
+							<div>
+								<CardTitle className='text-chart-3'>TICKET #ANTITS01</CardTitle>
+								<CardDescription></CardDescription>
+							</div>
+							<CardAction>
+								<Dialog>
+									<DialogTrigger>
+										<Button
+											className='bg-[#ebeff3]'
+											variant='outline'
+											style={{
+												boxShadow: `
       rgba(255, 255, 255, 0.7) 5px 5px 4px, 
       rgba(189, 194, 199, 0.75) 2px 2px 3px inset
     `,
-                      }}
-                    >
-                      28 April
-                    </Button>
-                  </DialogTrigger>
-                </Dialog>
-              </CardAction>
-            </div>
-          </CardHeader>
-          <CardContent>
-            <p className="font-semibold">This ticket created from student mobile app</p>
+											}}
+										>
+											28 April
+										</Button>
+									</DialogTrigger>
+								</Dialog>
+							</CardAction>
+						</div>
+					</CardHeader>
+					<CardContent>
+						<p className='font-semibold'>
+							This ticket created from student mobile app
+						</p>
 
-            <p>If you can this yes successfully mobile app an android</p>
-          </CardContent>
-          <CardFooter>
-            <div className="flex w-full justify-between items-center">
-
-
-              <p>1</p>
-              <CardAction >
-                <Dialog>
-                  <DialogTrigger className="text-sm font-medium text-primary shadow-md">
-                    <Button className="bg-[#ebeff3] w-30" variant="outline"
-                      style={{
-                        boxShadow: `
+						<p>If you can this yes successfully mobile app an android</p>
+					</CardContent>
+					<CardFooter>
+						<div className='flex w-full justify-between items-center'>
+							<p>1</p>
+							<CardAction>
+								<Dialog>
+									<DialogTrigger className='text-sm font-medium text-primary shadow-md'>
+										<Button
+											className='bg-[#ebeff3] w-30'
+											variant='outline'
+											style={{
+												boxShadow: `
        rgba(255, 255, 255, 0.7) 5px 5px 4px, 
        rgba(189, 194, 199, 0.75) 2px 2px 3px inset
     `,
-                      }}
-                    >Opend</Button>
-
-                  </DialogTrigger>
-                </Dialog>
-              </CardAction>
-            </div>
-          </CardFooter>
-        </Card>
-
-      </div>
-    </div>
-	)
+											}}
+										>
+											Opend
+										</Button>
+									</DialogTrigger>
+								</Dialog>
+							</CardAction>
+						</div>
+					</CardFooter>
+				</Card>
+			</div>
+		</div>
+	);
 };
 
 export default Tickets;
