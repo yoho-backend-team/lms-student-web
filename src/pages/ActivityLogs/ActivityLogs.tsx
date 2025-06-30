76a42440e00145fbafd229cf5343952707cfb694
Prakash-yoho <prakash.yohotechnologies@gmail.com>
2025-06-30 12:45:25 +0530

    Activity page corrections

--- src/pages/ActivityLogs/ActivityLogs.tsx
+++ src/pages/ActivityLogs/ActivityLogs.tsx
@@ -11,10 +11,20 @@ import { useNavigate } from 'react-router-dom';
 const ActivityLogs = () => {
 
 	const [handleFilter, setHandleFilter] = useState(false);
-	const [date, setDate] = useState<Date | undefined>(undefined);
+	const [fromDate, setFromDate] = useState<any | undefined>(undefined);
+	const [toDate, setToDate] = useState<any | undefined>(undefined);
 	const [showFromCalendar, setShowFromCalendar] = useState(false);
 	const [showToCalendar, setShowToCalendar] = useState(false);
-	const navigate = useNavigate()
+	const navigate = useNavigate();
+
+	const formatDate = (date: Date | undefined) => {
+        if (!date) return 'DD-MM-YYYY';
+        const day = date.getDate().toString().padStart(2, '0');
+        const month = (date.getMonth() + 1).toString().padStart(2, '0');
+        const year = date.getFullYear();
+        return `${day}-${month}-${year}`;
+    }
+
 	const activityLogs = [
 		{
 			id: 1,
@@ -42,165 +52,162 @@ const ActivityLogs = () => {
 		},
 	]
 
-	
+
 
 
 	return (
-		<div className='py-6 flex gap-6'>
-
-			<div className='w-3/4'>
-
-				<nav className='flex items-center gap-6'>
-					<div className='p-2 rounded-lg cursor-pointer'
-						style={{
-							boxShadow: `
+		<div>
+			<nav className='flex items-center gap-6 py-6'>
+				<div className='p-2 rounded-lg cursor-pointer'
+					style={{
+						boxShadow: `
       							rgba(255, 255, 255, 0.7) 5px 5px 4px, 
       							rgba(189, 194, 199, 0.75) 2px 2px 3px inset`,
-						}}
-						onClick={()=>navigate(-1)}
-						>
-						<img src={backBtn} alt="BackBtn" />
-					</div>
-					<h1 style={{ ...FONTS.heading_01 }}>Activity Log</h1>
-				</nav>
+					}}
+					onClick={() => navigate(-1)}
+				>
+					<img src={backBtn} alt="BackBtn" />
+				</div>
+				<h1 style={{ ...FONTS.heading_02 }}>Activity Log</h1>
+			</nav>
+			<div className=' flex lg:flex-row gap-5 md:flex-col-reverse'>
+					<div className=' lg:w-3/4 md:w-full relative'>
 
-				<div className='mt-10 relative'>
-					<div className='btnshadow h-[100%] w-3 rounded-2xl  absolute left-[29%] text-transparent'> l</div>
-
-					{activityLogs.map((data) => (
+						{activityLogs.map((data) => (
 
 
-						<section key={data.id} className='flex justify-between items-start py-6 gap-12 my-8'>
-							<div className='w-[240px]'>
-								<p style={{ ...FONTS.para_01 }}>{data.date}</p>
-							</div>
+							<section key={data.id} className='flex justify-between items-start py-6 gap-12 my-4 relative'>
+								<div className='w-[240px]'>
+									<p style={{ ...FONTS.heading_06 }}>{data.date}</p>
+								</div>
 
-							<div className='grid gap-6 w-3/4'>
-								<h3 className='pl-14' style={{ ...FONTS.para_01, color: COLORS.text_title }}>{data.title}</h3>
+								<div className='grid gap-6 w-3/4 relative pb-10'>
 
-								<section className='flex items-center gap-8'>
-									<div className='relative'>
-										<img src={vector_H} alt="Vector-H" />
-										<div
-											className='h-8 w-8 rounded-full absolute right-0 top-[-5px]'
-											style={{
-												background: COLORS.green_text,
+									<div className='btnshadow h-[100%] w-3 rounded-2xl  absolute left-0 text-transparent'> l</div>
+									<h3 className='pl-14' style={{ ...FONTS.heading_05, color: COLORS.text_title }}>{data.title}</h3>
+
+									<section className='flex items-center gap-8 relative'>
+										<div className='relative'>
+											<div className='h-9 w-9 rounded-full flex p-1 absolute left-[-10px] top-[-5px]' style={{
+												background: COLORS.blue_user,
 												boxShadow:
 													'rgba(189, 194, 199, 0.7) 2px 5px 4px,rgba(255, 255, 255, 0.4) 3px 2px 2px inset'
-											}}
-										></div>
+											}}>
+												<img src={User} alt="User-icon" />
+											</div>
+											<img src={vector_H} alt="Vector-H" />
+											<div
+												className='h-8 w-8 rounded-full absolute right-0 top-[-5px]'
+												style={{
+													background: COLORS.green_text,
+													boxShadow:
+														'rgba(189, 194, 199, 0.7) 2px 5px 4px,rgba(255, 255, 255, 0.4) 3px 2px 2px inset'
+												}}
+											></div>
 
-										<div className='h-8 w-8 rounded-full flex p-1 absolute left-[-8px] top-[-5px]' style={{
-											background: COLORS.blue_user,
-											boxShadow:
-												'rgba(189, 194, 199, 0.7) 2px 5px 4px,rgba(255, 255, 255, 0.4) 3px 2px 2px inset'
-										}}>
-											<img src={User} alt="User-icon" />
 										</div>
-									</div>
 
-									<div className='p-3 custom-inset-shadow'>
-										<p style={{ ...FONTS.heading_05, color: COLORS.green_text }}>{data.description}</p>
-									</div>
-								</section>
-							</div>
-						</section>
-					)
+										<div className='p-3 custom-inset-shadow'>
+											<p style={{ ...FONTS.heading_07, color: COLORS.green_text }}>{data.description}</p>
+										</div>
+									</section>
+								</div>
+							</section>
+						)
 
-					)}
-				</div>
+						)}
+					</div>
 
-			</div>
+				<div className='lg:w-[400px] md:w-fit'>
+					<div className='flex lg:justify-end md:items-start cursor-pointer'>
+						<img src={filter} alt="" className='h-8 text-end' onClick={() => {
+							setHandleFilter(!handleFilter);
+							setShowFromCalendar(false);
+							setShowToCalendar(false)
+						}} />
+					</div>
 
-			<div className='w-1/4'>
-				<div className='flex justify-end cursor-pointer'>
-					<img src={filter} alt="" className='h-8 text-end' onClick={() => {
-						setHandleFilter(!handleFilter);
-						setShowFromCalendar(false);
-						setShowToCalendar(false)
-					}} />
-				</div>
+					{handleFilter &&
+						<div className='grid md:grid-cols-2 lg:grid-cols-1'>
 
-				{handleFilter &&
-
-
-					<div>
-
-						<section className='flex gap-6 justify-end mt-4'>
-							<div>
-								<p style={{ ...FONTS.heading_07 }}>From</p>
-								<button className='p-2 rounded-lg'
-									onClick={() => {
-										setShowFromCalendar(!showFromCalendar);
-										setShowToCalendar(false); // close 'To' calendar if open
-									}}
-									style={{
-										...FONTS.heading_07,
-										boxShadow: `
+							<section className='flex gap-6 lg:justify-end mt-4'>
+								<div>
+									<p style={{ ...FONTS.heading_07 }}>From</p>
+									<button className='p-2 rounded-lg'
+										onClick={() => {
+											setShowFromCalendar(!showFromCalendar);
+											setShowToCalendar(false); // close 'To' calendar if open
+										}}
+										style={{
+											...FONTS.heading_07,
+											boxShadow: `
       							rgba(255, 255, 255, 0.7) 5px 5px 4px, 
       							rgba(189, 194, 199, 0.75) 2px 2px 3px inset`,
-									}}>DD-MM-YYYY</button>
-							</div>
+										}}>{fromDate || "DD-MM-YYYY"}</button>
+								</div>
 
-							<div>
-								<p style={{ ...FONTS.heading_07 }}>To</p>
-								<button className='p-2 rounded-lg'
-									onClick={() => {
-										setShowToCalendar(!showToCalendar);
-										setShowFromCalendar(false); // close 'From' calendar if open
-									}}
-									style={{
-										...FONTS.heading_07,
-										boxShadow: `
+								<div>
+									<p style={{ ...FONTS.heading_07 }}>To</p>
+									<button className='p-2 rounded-lg'
+										onClick={() => {
+											setShowToCalendar(!showToCalendar);
+											setShowFromCalendar(false); // close 'From' calendar if open
+										}}
+										style={{
+											...FONTS.heading_07,
+											boxShadow: `
       							rgba(255, 255, 255, 0.7) 5px 5px 4px, 
       							rgba(189, 194, 199, 0.75) 2px 2px 3px inset`,
-									}}>DD-MM-YYYY</button>
-							</div>
-						</section>
+										}}>{toDate || "DD-MM-YYYY"}</button>
+								</div>
+							</section>
 
-					</div>
-				}
+							{showFromCalendar && (
+								<div className=' mt-6 p-1 rounded-lg ' style={{
+									boxShadow:
+										'rgba(189, 194, 199, 0.7) 2px 5px 4px,rgba(255, 255, 255, 0.4) 3px 2px 2px inset'
+								}}>
+									<h2 className="text-xl font-semibold px-3 mt-4" style={{ ...FONTS.heading_02 }}>From Calendar</h2>
+									<Calendar
+										mode="single"
+										selected={fromDate}
+										onSelect={(selectedDate) => {
+											const fromDate = formatDate(selectedDate)
+											setFromDate(fromDate);
+											setShowFromCalendar(false); // hide after selection
+										}}
+										className="rounded-lg bg-gray-100 w-full **:gap-2"
+										style={{ backgroundColor: COLORS.bg_Colour }}
+										captionLayout="dropdown"
+									/>
+								</div>
+							)}
+		
+							{showToCalendar && (
+								<div className=' mt-6 p-1 rounded-lg ' style={{
+									boxShadow:
+										'rgba(189, 194, 199, 0.7) 2px 5px 4px,rgba(255, 255, 255, 0.4) 3px 2px 2px inset'
+								}}>
+									<h2 className="text-xl font-semibold px-3 mt-4" style={{ ...FONTS.heading_02 }}>To Calendar</h2>
+									<Calendar
+										mode="single"
+										selected={toDate}
+										onSelect={(selectedDate) => {
+											const toDate = formatDate(selectedDate)
+											setToDate(toDate);
+											setShowToCalendar(false); // hide after selection
+										}}
+										className="rounded-lg bg-gray-100 w-full **:gap-2"
+										style={{ backgroundColor: COLORS.bg_Colour }}
+										captionLayout="dropdown"
+									/>
+								</div>
+							)}
+						</div>
+					}
 
 
-				{showFromCalendar && (
-					<div className=' mt-6 p-1 rounded-lg absolute' style={{
-						boxShadow:
-							'rgba(189, 194, 199, 0.7) 2px 5px 4px,rgba(255, 255, 255, 0.4) 3px 2px 2px inset'
-					}}>
-						<h2 className="text-xl font-semibold px-3 mt-4" style={{ ...FONTS.heading_02 }}>From Calendar</h2>
-						<Calendar
-							mode="single"
-							selected={date}
-							onSelect={(selectedDate) => {
-								setDate(selectedDate);
-								setShowFromCalendar(false);
-							}}
-							className="rounded-lg bg-gray-100 w-full **:gap-1"
-							style={{ backgroundColor: COLORS.bg_Colour }}
-							captionLayout="dropdown"
-						/>
-					</div>
-				)}
-
-				{showToCalendar && (
-					<div className=' mt-6 p-1 rounded-lg absolute' style={{
-						boxShadow:
-							'rgba(189, 194, 199, 0.7) 2px 5px 4px,rgba(255, 255, 255, 0.4) 3px 2px 2px inset'
-					}}>
-						<h2 className="text-xl font-semibold px-3 mt-4" style={{ ...FONTS.heading_02 }}>To Calendar</h2>
-						<Calendar
-							mode="single"
-							selected={date}
-							onSelect={(selectedDate) => {
-								setDate(selectedDate);
-								setShowToCalendar(false); // hide after selection
-							}}
-							className="rounded-lg bg-gray-100 w-full **:gap-1"
-							style={{ backgroundColor: COLORS.bg_Colour }}
-							captionLayout="dropdown"
-						/>
-					</div>
-				)}
+				</div>
 			</div>
 		</div>
 	);
