b5668f3874e9b21e7bee3f0408172a6beef37d46
mugilane.cse <55045435+Mugilane@users.noreply.github.com>
2025-06-30 11:19:42 +0530

    merge main

--- src/components/dashboard/Calendar.tsx
+++ src/components/dashboard/Calendar.tsx
@@ -5,13 +5,13 @@ import { COLORS, FONTS } from '@/constants/uiConstants'
 const CalendarDash: React.FC = () => {
     const date = new Date()
     return (
-        <div className='divshadow w-full rounded-[16px] p-2'>
+        <div className='divshadow w-full h-full rounded-[16px] p-2'>
             <h1 style={{ ...FONTS.heading_02 }}>Calendar</h1>
             <Calendar
                 mode="single"
                 selected={date}
                 // onSelect={setDate}
-                className="rounded-md border shadow-lg bg-gray-100 w-full **:gap-2 "
+                className="rounded-md border shadow-lg bg-gray-100 w-full **:mt-2 **:gap-7 "
                 style={{ backgroundColor: COLORS.bg_Colour }}
                 captionLayout="dropdown"
             />
