55f656bfccc3bfd5b413a19dd9bd4f9c0c8b767e
Vel vinoth <sm.vinothsm@gmail.com>
2025-06-30 13:19:41 +0530

    updates

--- src/components/courses/coursebutton.tsx
+++ src/components/courses/coursebutton.tsx
@@ -9,32 +9,37 @@ function CourseButton() {
 
 
   return (
-    <div>
+<div className="flex justify-center gap-6 mb-12">
+  <Button 
+    onClick={() => navigate('/about/mernstack')}
+    className=" w-60 bg-[#EBEFF3] hover:bg-[#EBEFF3] focus:bg-[#EBEFF3] hover:bg-gradient-to-r focus:bg-gradient-to-r from-[#7B00FF] to-[#B200FF] hover:text-white focus:text-white text-[#444] px-6 py-2 rounded-md shadow-[3px_3px_5px_rgba(255,255,255,0.7),inset_2px_2px_3px_rgba(189,194,199,0.75)] focus:outline-none focus:ring-2 focus:ring-[#7B00FF] focus:ring-opacity-50 transition-all duration-200"
+  >
+    About
+  </Button>
+  
+  <Button 
+    onClick={() => navigate('/note_materials')}
+    className=" w-60 bg-[#EBEFF3] hover:bg-[#EBEFF3] focus:bg-[#EBEFF3] hover:bg-gradient-to-r focus:bg-gradient-to-r from-[#7B00FF] to-[#B200FF] hover:text-white focus:text-white text-[#444] px-6 py-2 rounded-md shadow-[3px_3px_5px_rgba(255,255,255,0.7),inset_2px_2px_3px_rgba(189,194,199,0.75)] focus:outline-none focus:ring-2 focus:ring-[#7B00FF] focus:ring-opacity-50 transition-all duration-200"
+  >
+    Class Notes & Materials
+  </Button>
+  
+  <Button 
+    onClick={() => navigate('/task_projects')}
+    className=" w-60 bg-[#EBEFF3] hover:bg-[#EBEFF3] focus:bg-[#EBEFF3] hover:bg-gradient-to-r focus:bg-gradient-to-r from-[#7B00FF] to-[#B200FF] hover:text-white focus:text-white text-[#444] px-6 py-2 rounded-md shadow-[3px_3px_5px_rgba(255,255,255,0.7),inset_2px_2px_3px_rgba(189,194,199,0.75)]  focus:outline-none focus:ring-2 focus:ring-[#7B00FF] focus:ring-opacity-50 transition-all duration-200"
+  >
+    Task & Projects
+  </Button>
+  
+  <Button 
+    onClick={() => navigate('/course_track')}
+    className=" w-60 bg-[#EBEFF3] hover:bg-[#EBEFF3] focus:bg-[#EBEFF3] hover:bg-gradient-to-r focus:bg-gradient-to-r from-[#7B00FF] to-[#B200FF] hover:text-white focus:text-white text-[#444] px-6 py-2 rounded-md shadow-[3px_3px_5px_rgba(255,255,255,0.7),inset_2px_2px_3px_rgba(189,194,199,0.75)] focus:outline-none focus:ring-2 focus:ring-[#7B00FF] focus:ring-opacity-50 transition-all duration-200"
+  >
+    Course Track
+  </Button>
+</div>
 
-      <div className="flex justify-center gap-4 mb-12">
-        <Button onClick={() => {
-          navigate('/about/mernstack')
-          // setpagename('About')
-        }} className="bg-[#EBEFF3] hover:bg-[#EBEFF3]  hover:bg-gradient-to-r  from-[#7B00FF] to-[#B200FF] hover:text-white text-[#444] px-6 py-2 rounded-md shadow-[3px_3px_5px_rgba(255,255,255,0.7),inset_2px_2px_3px_rgba(189,194,199,0.75)]">
-          About
-        </Button>
-        <Button onClick={() => {
-          navigate('/note_materials')
-          // setpagename('Class Notes & Materials')
-        }} className="bg-[#EBEFF3] hover:bg-[#EBEFF3]  hover:bg-gradient-to-r  from-[#7B00FF] to-[#B200FF] hover:text-white text-[#444] px-6 py-2 rounded-md shadow-[3px_3px_5px_rgba(255,255,255,0.7),inset_2px_2px_3px_rgba(189,194,199,0.75)]">
-          Class Notes & Materials</Button>
-        <Button onClick={() => {
-          navigate('/task_projects')
-          // setpagename('Task & Projects')
-        }} className="bg-[#EBEFF3] hover:bg-[#EBEFF3]  hover:bg-gradient-to-r  from-[#7B00FF] to-[#B200FF] hover:text-white text-[#444]  px-6 py-2 rounded-md shadow-[3px_3px_5px_rgba(255,255,255,0.7),inset_2px_2px_3px_rgba(189,194,199,0.75)]">
-          Task & Projects</Button>
-        <Button onClick={() => {
-          navigate('/course_track')
-          // setpagename('Course Track')
-        }} className="bg-[#EBEFF3] text-[#444]  hover:bg-gradient-to-r  from-[#7B00FF] to-[#B200FF] hover:text-white px-6 py-2 rounded-md shadow-[3px_3px_5px_rgba(255,255,255,0.7),inset_2px_2px_3px_rgba(189,194,199,0.75)]">Course Track</Button>
-      </div>
-
-    </div>
+    
   )
 }
 
