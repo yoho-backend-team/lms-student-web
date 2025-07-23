8a30070d9ed24ed4fb9581b8c435eb0c885eef42
mugilane.cse <55045435+Mugilane@users.noreply.github.com>
2025-07-23 10:22:20 +0530

    update

--- src/pages/Courses/Courses.tsx
+++ src/pages/Courses/Courses.tsx
@@ -3,27 +3,30 @@ import { selectCourse } from '@/features/Course/reducer/selector';
 import { getStudentcourse } from '@/features/Course/reducer/thunks';
 import { useInstituteData } from '@/hooks/DashboardData/useInstitute';
 import type { AppDispatch } from '@/store/store';
+import { GetLocalStorage } from '@/utils/helper';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
 
 const Courses = () => {
   const dispatch = useDispatch<AppDispatch>();
-  const coursedata = useSelector(selectCourse); 
+  const coursedata = useSelector(selectCourse);
   console.log('Redux course data:', coursedata);
 
   const instituteuuid = useInstituteData();
-      console.log('institue details',instituteuuid);
+  console.log('institue details', instituteuuid);
 
   useEffect(() => {
     const fetchData = async () => {
-      try { 
-      const params = {
-        instituteuuid : useInstituteData().instituteDetails,
-        branchuuid : '90c93163-01cf-4f80-b88b-4bc5a5dd8ee4',
-        courseId : '67a0bd83a0af9570a36c499d'
-      }
-        await dispatch(getStudentcourse(params)); 
+      const instituteId = GetLocalStorage('instituteId')
+      const branchId = GetLocalStorage('branchId')
+      try {
+        const params = {
+          instituteuuid: instituteId,
+          branchuuid: branchId,
+          courseId: '67a0bd83a0af9570a36c499d'
+        }
+        await dispatch(getStudentcourse(params));
       } catch (error) {
         console.error('Course fetch error:', error);
       }
@@ -32,13 +35,13 @@ const Courses = () => {
     fetchData();
   }, [dispatch]);
 
-	return (
+  return (
 
-		<div>
-			<MainCourse />
-		</div>
+    <div>
+      <MainCourse />
+    </div>
 
-	)
+  )
 
 };
 
--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 import React, { useEffect } from 'react';
 // import style from './style.module.css'
 import InstituteDetails from '@/components/dashboard/InstituteDetails';
