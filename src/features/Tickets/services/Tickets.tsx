a36fdd7cf193a1964d9432eb3dcf745d7e351b64
suryas-1805 <suryas18051998@gmail.com>
2025-07-23 16:20:54 +0530

    changes

--- src/features/Tickets/services/Tickets.tsx
+++ src/features/Tickets/services/Tickets.tsx
@@ -1,19 +1,15 @@
-import Client from "../../../api/index"
+import Client from '../../../api/index';
 
-export const getticketdata = async (params:any) => {
-    
-      const response = await Client.student.ticket.get(params)
-      if(response){
-    return response;
-      }
-    
-}
-export const createticketdata = async (data:any,params:any) => {
-    
-      const response = await Client.student.ticket.create(data,params)
-      if(response){
-    return response;
-      }
-    
-}
-    
+export const getticketdata = async (params: any) => {
+	const response = await Client.student.ticket.get(params);
+	if (response) {
+		return response;
+	}
+};
+
+export const createticketdata = async (data: any, params: any) => {
+	const response = await Client.student.ticket.create(data, params);
+	if (response) {
+		return response;
+	}
+};
