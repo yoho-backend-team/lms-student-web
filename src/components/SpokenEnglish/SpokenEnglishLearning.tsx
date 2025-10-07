e5fba3a122073bd03e51757abcd91960c4184c01
ARAVINTH-12345 <aravinth.yoho@gmail.com>
2025-10-07 14:48:44 +0530

    completed

--- src/components/SpokenEnglish/SpokenEnglishLearning.tsx
+++ src/components/SpokenEnglish/SpokenEnglishLearning.tsx
@@ -1,4 +1,3 @@
-/* eslint-disable @typescript-eslint/no-explicit-any */
 import { useState, useEffect, useRef } from 'react';
 import { Card } from '../ui/card';
 import { Button } from '../ui/button';
