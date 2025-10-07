55864ab3965ef64773bd6f55996705dfee643bbd
sowmiya-krish123 <sowmiya.yoho@gmail.com>
2025-10-07 18:41:29 +0530

    student

--- src/components/dashboard/ui/GridProgress.tsx
+++ src/components/dashboard/ui/GridProgress.tsx
@@ -19,14 +19,14 @@ const GridProgress: React.FC<propstype> = ({ title, value, icon }) => {
         offlineclass: "stroke-[#E67123]",
     }
     return (
-        <div className="flex flex-row justify-between px-4 py-2">
+        <div className="flex flex-row justify-between px-2 py-3 ">
             <div className="flex flex-col">
                 <h3
                     style={{ ...FONTS.heading_05 }}
-                    className="text-[18px]">{title ?? "title"}</h3>
+                    className="!text-[13px]">{title ?? "title"}</h3>
                 <img src={dashicons[icon as keyof typeof dashicons]} alt="" width={40} height={40} className="mt-2" />
             </div>
-            <div className="w-[60px] h-[60px]">
+            <div className="w-[60px] h-[60px] ">
                 <CircularProgressWithLabelDemo value={value} color={color[icon as keyof typeof color]} />
             </div>
         </div >
