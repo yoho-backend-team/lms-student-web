139259ab5631f3c9823b5c9b2ad1d46ad91599e3
Raajes Kumar K S <raajeskumar.yoho@gmail.com>
2025-11-21 14:23:41 +0530

    update

--- src/pages/CourseId/CourseId.tsx
+++ src/pages/CourseId/CourseId.tsx
@@ -2,7 +2,7 @@ import { useParams } from 'react-router-dom';
 
 const CourseId = () => {
 	const { id } = useParams();
-	return <div>Course -{id}</div>;
+	return <div>Course - {id}</div>;
 };
 
 export default CourseId;
